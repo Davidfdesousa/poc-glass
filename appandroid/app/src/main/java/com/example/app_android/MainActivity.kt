77dcc1198b8068372cfeac2159bf7dc532c50653
David Ferreira <davidfdesousa@hotmail.com>
2025-11-20 12:56:53 -0300

    feat: criando app android

---
+++ appandroid/app/src/main/java/com/example/app_android/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.app_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.app_android.ui.theme.AppandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppandroidTheme {
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    OrangeButton()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun OrangeButton() {
+    Button(
+        onClick = { /* TODO: ação do clique */ },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFFF9800), // laranja
+            contentColor = Color.White
+        )
+    ) {
+        Text(text = "Botão laranja2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OrangeButtonPreview() {
+    AppandroidTheme {
+        OrangeButton()
+    }
+}
